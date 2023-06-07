84a52f52bbba464539897fd2a9e6d7144a5dc660
Hieu Phan <phanvanhieu16032003@gmail.com>
2023-06-07 16:52:51 +0700

    lesson3

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,8 +1,11 @@
 import React from 'react';
 class MyComponent extends React.Component {
+    state = { name: "Hieu Phan Dev", address: "HCM", age: 20 };
     render() {
-        return(
-            <div>Hello</div>
+        return (
+            <div>
+                My name is {this.state.name} and I'm form {this.state.address} now I'm {this.state.age} years old
+            </div>
         );
     }
 }
