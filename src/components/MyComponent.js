99bfcc3e5ecee8451cb10e292963c3f699c0c6ca
unknown <phanvanhieu16032003@gmail.com>
2023-06-09 16:00:25 +0700

    lesson3

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,36 @@
 import React from 'react';
 class MyComponent extends React.Component {
     state = { name: "Hieu Phan Dev", address: "HCM", age: 20 };
+
+    handleclick(event){
+        this.setState({
+            name: "Dev",
+            age: 19
+        })
+    }
+    handleOnChangeInput(event){
+        this.setState({
+            name: event.target.value,
+            age: 19
+        })
+    }
+    handleOnSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state)
+    }
     render() {
         return (
             <div>
                 My name is {this.state.name} and I'm form {this.state.address} now I'm {this.state.age} years old
+                <form onSubmit={(event) => {this.handleOnSubmit(event)}}>
+                    <input
+                    type='text'
+                    onChange={(event) => {this.handleOnChangeInput(event)}}
+                    />
+                    <button>submit</button>
+                </form>
             </div>
+            
         );
     }
 }
